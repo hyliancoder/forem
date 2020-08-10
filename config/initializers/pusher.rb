690d1b4d05028503133a64d49bf6f0f80c6ac551
mstruve <mollylbs@gmail.com>
2020-08-10 16:39:43 -0500

    see all push vars

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,5 +1,11 @@
 require "pusher/push_notifications"
 
+puts "PUSHER_APP_ID #{ApplicationConfig["PUSHER_APP_ID"]}"
+puts "PUSHER_KEY #{ApplicationConfig["PUSHER_KEY"]}"
+puts "PUSHER_SECRET #{ApplicationConfig["PUSHER_SECRET"]}"
+puts "PUSHER_CLUSTER #{ApplicationConfig["PUSHER_CLUSTER"]}"
+
+
 Pusher.app_id = ApplicationConfig["PUSHER_APP_ID"]
 Pusher.key = ApplicationConfig["PUSHER_KEY"]
 Pusher.secret = ApplicationConfig["PUSHER_SECRET"]
